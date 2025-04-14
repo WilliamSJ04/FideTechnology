4fe0dc6aa15dad81b6444057746a2cef130ecb4f
BrunoSR-299 <135384671+BrunoSR-299@users.noreply.github.com>
2025-04-14 15:38:14 -0600

    Update ProductosController.php

--- Controller/ProductosController.php
+++ Controller/ProductosController.php
@@ -1,6 +1,10 @@
 <?php
 include_once $_SERVER["DOCUMENT_ROOT"] . "/FideTechnology/Model/ProductosModel.php";
 
+if ($_SESSION['usuario']['rol'] !== 'admin') {
+      header('Location: ?controlador=Login&accion=accesoDenegado');
+      exit;
+
 function ConsultarProductos()
 {
     return ConsultarProductosModel();
